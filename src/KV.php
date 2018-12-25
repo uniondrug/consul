b3eb97c71d735bcb31114e0457173d927ded7af6
付义兵 <websearch@163.com>
2018-12-25 09:56:10 +0800

    no message

---
+++ src/KV.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author wsfuyibing <websearch@163.com>
+ * @date   2018-10-30
+ */
+namespace Uniondrug\Consul;
+
+/**
+ * 操作Consul K/V
+ * @package Uniondrug\Consul
+ */
+class KV
+{
+    /**
+     * 初始化配置文件
+     */
+    public static function initConfig()
+    {
+        echo "init configuration with Consul::KV\n";
+    }
+}
